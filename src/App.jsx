8f325fb904af78994324762daaebff5ecd116c46
Nixon <nixonkoech200@gmail.com>
2024-10-21 21:54:26 +0300

    Add three more components to the parent component for routing and rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/Homepage';
 import HabitManagement from './pages/HabitManagement';
+import LoginForm from './pages/LoginForm';
+import AboutMe from './pages/AboutMe';
+import Resources from './pages/Resources';
 
 function App() {
   return (
@@ -10,6 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/habit-management" element={<HabitManagement />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="/resources" element={<Resources />} />
+
       </Routes>
     </Router>
 
