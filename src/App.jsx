d80dba6c2bb76b5cf1e67ef2bfd5d26e45847a49
Nixon <nixonkoech200@gmail.com>
2024-10-21 17:23:48 +0300

    Implement routing for HomePage and HabitManagement pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import HomePage from './pages/Homepage'
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/Homepage';
+import HabitManagement from './pages/HabitManagement';
 
 function App() {
   return (
-    <>
-    <HomePage />
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/habit-management" element={<HabitManagement />} />
+      </Routes>
+    </Router>
 
-export default App
+  );
+};
+
+export default App;
