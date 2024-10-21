121a1211e31d76ed594f0a534d5811b3c049e566
Nixon <nixonkoech200@gmail.com>
2024-10-21 21:51:08 +0300

    Enable responsive behaviour for Nav Links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <nav className="bg-blue-500 p-4">
-            <ul className="flex justify-around">
-                <li>
+        <nav className="bg-blue-500 p-4 fixed top-0 left-0 w-full z-10 shadow-md">
+            <div className="container mx-auto p-4 flex justify-between items-center">
+                {/* Brand Logo or Name */}
+                <Link to="/" className="text-white text-xl left-0 font-bold">
+                    TrackiBits
+                </Link>
+
+                {/* Hamburger Icon for Mobile */}
+                <button
+                    className="text-white md:hidden"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    <svg
+                        className="w-6 h-6"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16m-7 6h7'}
+                        />
+                    </svg>
+                </button>
+
+                {/* Nav Links for Desktop & Mobile */}
+                
+            <ul className={`md:flex space-x-6 items-center ${isOpen ? 'block' : 'hidden'} md:block ml-auto`}>
+                 <li>
                     <Link to="/" className="text-white hover:text-gray-200">
                         Home
                     </Link>
@@ -15,7 +46,24 @@ const NavBar = () => {
                         Habit Management
                     </Link>
                 </li>
+                <li>
+                    <Link to="/login" className="text-white hover:text-gray-200">
+                        Login
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/about" className="text-white hover:text-gray-200">
+                        About Me
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/resources" className="text-white hover:text-gray-200">
+                        Resources
+                    </Link>
+                </li>
+            
             </ul>
+         </div>
         </nav>
     );
 };
