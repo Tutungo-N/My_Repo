5dee602cea6885961d6fd9d09ea5d2a05f32e853
Nixon <nixonkoech200@gmail.com>
2024-10-25 15:16:37 +0300

    Add Layout.jsx to App.jsx as a reusable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout';
 import HomePage from './pages/Homepage';
 import HabitManagement from './pages/HabitManagement';
 import SignupForm from './pages/SignUpForm';
@@ -11,6 +12,7 @@ import Resources from './pages/Resources';
 function App() {
   return (
     <Router>
+      <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/habit-management" element={<HabitManagement />} />
@@ -18,8 +20,8 @@ function App() {
         <Route path="/signup" element={<SignupForm />} />  
         <Route path="/about" element={<AboutMe />} />
         <Route path="/resources" element={<Resources />} />
-
       </Routes>
+      </Layout>
     </Router>
 
   );
