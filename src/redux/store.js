d964ffedfd7965e40e6f822b58a12adb2b77c4e4
Nixon <nixonkoech200@gmail.com>
2024-12-17 19:06:00 +0300

    Add Redux-Persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,8 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // This would default to localStorage for web
+import { combineReducers } from '@reduxjs/toolkit';
+//import persistReducer from 'redux-persist/es/persistReducer';
 
 // Create user slice for managing login state
 const userSlice = createSlice({
@@ -21,10 +25,31 @@ const userSlice = createSlice({
 
 export const { setUser, logout } = userSlice.actions;
 
-const store = configureStore({
+// Persist Configuration
+const persistConfig = {
+    key: 'root',
+    storage, // To default to storage
+    whitelist: ['user'], // Only persist 'user' state 
+};
+
+// Combine Reducers (in case you have more slices in the future)
+const rootReducer = combineReducers({
+    user: userSlice.reducer,
+});
+/*const store = configureStore({
     reducer: {
         user: userSlice.reducer,
     },
+});*/
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure Store with Persisted Reducer
+const store = configureStore({
+    reducer: persistedReducer,
+
 });
+
+export const persistor = persistStore(store);
 
 export default store;
